f93825efc1c114ffe44d2b7a73a05fbb7ba2de37
Christopher Coker <ccoker@intergate.com>
2014-11-24 16:23:14 -0500

    modified

--- room/30.zon/obj/match.c
+++ room/30.zon/obj/match.c
@@ -11,18 +11,35 @@ static void create() {
     SetAdjectives( ({ "wooden" }) );
     SetShort("a wooden match");
     SetLong("A wooden match that might light if you strike it.");
+    SetMass(1);
     SetRadiantLight(2);
     SetStrikeChance(50);
     SetMinHeat(10);
     SetFuelRequired(1);
-    SetMaxFuel(10);
-    SetFuelAmount(10);
+    SetMaxFuel(2);
+    SetFuelAmount(2);
     SetRefuelable(0);
     SetMass(1);
-    SetBaseCost("gold",1);
-    SetBurntValue(1);
+    SetBaseCost("gold", 0);
+    SetBurntValue(0);
 }
 
 void init(){
     ::init();
 }
+
+/* If you drop a match it goes out. */
+
+mixed eventMove(mixed dest){
+   
+    if(objectp(dest) && living(dest)){
+        return match::eventMove(dest);
+    }
+    if(objectp(dest) && !living(dest) && this_object()->GetLit() ==1){
+        eventExtinguish();
+        return match::eventMove(dest);
+    }
+    if(objectp(dest) && !living(dest) && this_object()->GetLit() !=1){
+        return match::eventMove(dest);
+    } 
+} 
