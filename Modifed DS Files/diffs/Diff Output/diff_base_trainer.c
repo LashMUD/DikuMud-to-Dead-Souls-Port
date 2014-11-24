5b4c89eb9d658cb1a6cbce539d2c4edd29982b54
Christopher Coker <ccoker@intergate.com>
2014-11-24 17:52:34 -0500

    added

--- Modifed DS Files/diffs/Diff Output/diff_base_trainer.c
+++ Modifed DS Files/diffs/Diff Output/diff_base_trainer.c
@@ -1,12 +1,18 @@
-153a154,155
+10c10
+< #include "include/trainer.h"
+---
+> #include "/lib/include/trainer.h"
+153a154,156
+>        //added by Lash
 >         mapping myspells = me->GetSpellBook();
->         mapping playerspells = who->GetSpellBook();;
-160c162
+>         mapping playerspells = who->GetSpellBook();
+160c163,164
 <         if(!ob){
 ---
+>         //modified by Lash
 >         if(!ob || !myspells[skill]){
-168a171,175
-> 
+168a173,177
+>         //added by Lash
 >         if(playerspells[skill]){
 >             me->eventForce("say You already know that spell.");
 >             return 1;
