930b3c606196e3f52b3276694445801e6ddf6d60
Christopher Coker <ccoker@intergate.com>
2014-11-28 18:25:50 -0500

    added functionality

--- room/30.zon/npc/3094_wolf.c
+++ room/30.zon/npc/3094_wolf.c
@@ -147,7 +147,7 @@ int RemoveProp(){
 
     if(ob && ob->GetProperty(this_object())){
         ob->RemoveProperty(this_object());
-        tell_player(ob, "Property removed.");
+        //tell_player(ob, "Property removed.");
         return 1;
     }
    return 1;
