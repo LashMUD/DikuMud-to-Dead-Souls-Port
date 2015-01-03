329a00bfa741d221acd9cc9feecc4a000ad6b471
Christopher Coker <ccoker@intergate.com>
2015-01-03 10:22:32 -0500

    Increased meal strength

--- room/30.zon/meals/3004_local_bottle.c
+++ room/30.zon/meals/3004_local_bottle.c
@@ -28,7 +28,7 @@ void create(){
     SetVendorType(VT_DRINK);
     SetFlaskContents("local speciality");
     SetFlaskUses(8);
-    SetStrength(3);
+    SetStrength(6);
     SetMaxFlask(8); 
     SetNoCondition(1);
     SetMealType(MEAL_ALCOHOL);
