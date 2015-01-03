c4e024495ce55250217f07dcdbe3837cc2799cb8
Christopher Coker <ccoker@intergate.com>
2015-01-03 10:25:33 -0500

    new magic item Word of Recall scroll

---
+++ room/30.zon/obj/3042_scroll_recall.c
@@ -0,0 +1,38 @@
+#include <lib.h>
+#include <vendor_types.h>
+inherit LIB_ITEM;
+
+void read_scroll();
+
+void create(){
+    ::create();
+    SetKeyName("scroll");
+    SetId(({"scroll of recall", "recall"}));
+    SetAdjectives(({"none"}));
+    SetShort("a scroll of recall");
+    SetLong("The scroll has written a formulae of 'Word of Recall' upon it.\n"+
+            "read 'recall on scroll'" );
+    SetMass(1);
+    SetBaseCost("gold", 2000);
+    SetVendorType(VT_MAGIC);
+    SetRead( ([
+        "recall" : (:read_scroll:)
+        ]) );
+    SetLanguage("common");
+}
+
+void init(){
+    ::init();
+}
+
+void read_scroll(){
+    
+    tell_player(this_player(), "\nThe scroll crumbles into dust...\n");
+    this_player()->eventMoveLiving("/domains/diku-alfa/room/30.zon/3001.c",
+                                   this_player()->GetName()+" disappears!", 
+                                   this_player()->GetName()+" appears in the middle of the room.");
+    this_object()->eventDestruct();
+}
+    
+    
+    
