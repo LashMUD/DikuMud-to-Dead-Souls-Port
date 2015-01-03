47f0dc32e29083023f63e2048a15fa015937dbb9
Christopher Coker <ccoker@intergate.com>
2015-01-03 10:30:31 -0500

    Added comments

--- etc/trainer.c
+++ etc/trainer.c
@@ -1,6 +1,8 @@
 #include <lib.h>
 
+/* modified by Lash for inclusion of modified base_trainer.c */
 inherit "/domains/diku-alfa/etc/base_trainer.c";
+/* end mod */
 inherit LIB_SENTIENT;
 
 static void create(){
